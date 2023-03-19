dbed8215525106a50e0ecc472fec576bafefa227
joseyzambranov <joseyzambranovpe@gmail.com>
2023-03-19 13:31:07 -0500

    finished crud

--- routes/api.js
+++ routes/api.js
@@ -13,33 +13,22 @@ const { ObjectID } = require('mongodb');
 module.exports = function (app) {
 
   app.route('/api/books')
-    .get(function (req, res){
-      BookModel.aggregate([
-        {
-          $lookup: {
-            from: 'comments',
-            localField: '_id',
-            foreignField: 'bookId',
-            as: 'comments'
-          }
-        },
-        {
-          $project: {
-            _id: 1,
-            title: 1,
-            commentcount: { $size: "$comments" }
-          }
-        }
-      ]).exec((err,books)=>{
-        if(err){
-          return res.status(500).json({error:err.message});
-        }
-        return res.json(books)
-      })
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    })
-    
+  .get(function (req, res){
+    BookModel.find({}, function(err, books) {
+      if (err) {
+        return res.status(500).json({error: err.message});
+      }
+      const bookList = [];
+      books.forEach(function(book) {
+        const comments = book.commentcount.map(function(comment) {
+          return comment;
+        });
+        bookList.push({_id: book._id, title: book.title,comments:comments, commentcount: comments.length});
+      });
+      return res.json(bookList);
+    });
+  })
+
     .post(function (req, res){
       let book_title = req.body.title;
       console.log(book_title)
@@ -54,7 +43,11 @@ module.exports = function (app) {
         if (err) {
           res.send("There was an error saving in post");
         } else {
-          res.json(newBook);
+          const response = {
+            _id: newBook._id,
+            title: newBook.title
+          }
+          res.json(response);
         }
       });
        
@@ -62,6 +55,12 @@ module.exports = function (app) {
     })
     
     .delete(function(req, res){
+      BookModel.deleteMany({},function(err){
+        if(err){
+          return res.status(500).json({error:err.message})
+        }
+        return res.json("complete delete successful")
+      })
       //if successful response will be 'complete delete successful'
     });
 
@@ -95,11 +94,57 @@ module.exports = function (app) {
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
+      console.log({bookid})
+      console.log({comment})
+      if(!comment){
+        return res.json("missing required field comment")
+      }
+      BookModel.updateOne(
+        {_id:mongoose.Types.ObjectId(bookid)},
+        {$push:{"commentcount":comment}}
+        ).exec((err,result)=>{
+          if(err){
+            return res.status(500).json({error:err.message})
+          }
+          if(result.matchedCount==0){
+            return res.json("no book exists")
+          }
+          if(result){
+            BookModel.aggregate([
+              {$match:{_id:mongoose.Types.ObjectId(bookid)}},
+              {$group:{_id:"$_id",title:{$first:"$title"},comments:{$push:"$commentcount"}}}
+            ]).exec((err,result)=>{
+              if(err){
+                return res.status(500).json({error:err.message})
+              }
+              if(!result||result.length == 0){
+                return res.json("no book exists")
+              }
+              const book = result[0]
+              return res.json({
+                _id:book._id,
+                title:book.title,
+                comments:book.comments.flat()
+              })
+              
+            })
+          }
+        })
       //json res format same as .get
+        
     })
     
     .delete(function(req, res){
       let bookid = req.params.id;
+      BookModel.findByIdAndDelete(bookid,function(err,deleteBook){
+        if(err){
+          return res.status(500).json({error:err.message})
+        }
+        if(!deleteBook){
+          return res.json("no book exists")
+        }
+        return res.json("delete successful")
+      })
       //if successful response will be 'delete successful'
     });
   
